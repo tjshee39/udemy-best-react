70eaab5028159c90fad8dc37c436f1ce325fd64a
tjshee39 <gksh054@naver.com>
2024-04-19 10:57:37 +0900

    component, state, useRef, tailwind

---
+++ 01-starting-project 5/src/assets/components/NewTask.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const NewTask = ({onAdd, onDelete}) => {
+  const [enteredTask, setEnteredTask] = useState('')
+
+  const handleChange = (event) => {
+    setEnteredTask(event.target.value)
+  }
+
+  const handleClick = () => {
+    if (enteredTask.trim() === '') {
+      return
+    }
+
+    onAdd(enteredTask)
+    setEnteredTask('');
+  }
+
+  return (
+    <div className="flex items-center gap-4">
+      <input
+        type="text"
+        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        onChange={handleChange}
+        value={enteredTask}
+      />
+      <button
+        className="text-stone-700 hover:text-stone-950"
+        onClick={handleClick}>
+        Add Task
+      </button>
+    </div>
+  )
+}
+
+export default NewTask
