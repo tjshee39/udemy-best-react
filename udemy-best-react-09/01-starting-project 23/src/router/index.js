5e6ed7c887e6ecde942286bba870e69b1d4bff5e
tjshee39 <gksh054@naver.com>
2025-02-27 17:02:39 +0900

    init udemy-best-react-10

---
+++ udemy-best-react-09/01-starting-project 23/src/router/index.js
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import BlogPage, { loader as postsLoader } from '../pages/Blog'
+import HomePage from '../pages/Home'
+import PostPage, { loader as postLoader } from '../pages/Post'
+import RootLayout from '../pages/Root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'posts',
+        children: [
+          { index: true, element: <BlogPage />, loader: postsLoader },
+          { path: ':id', element: <PostPage />, loader: postLoader },
+        ],
+      },
+    ],
+  },
+])
+
+export default router
